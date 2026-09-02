ab710cff0f4a0132d029e95f8baac6133e81b6f4
Luca Rodrigaño <Lucarodrigano@gmail.com>
2026-09-02 17:12:13 -0300

    Correccion en inmueble

--- Models/Inmueble.cs
+++ Models/Inmueble.cs
@@ -1,4 +1,5 @@
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace inmobiliaria_grupo_9.Models
 {
@@ -23,17 +24,23 @@ namespace inmobiliaria_grupo_9.Models
         [Display(Name = "Precio por día")]
         public decimal PrecioXDia { get; set; }
 
+        [Column("Metros_Cuadrados")]
         [Display(Name = "Metros cuadrados")]
         public decimal MetrosCuadrados { get; set; }
 
+        [Column("Nro_Ambientes")]
         [Display(Name = "Nro. de ambientes")]
         public int NroAmbientes { get; set; }
 
+        [Column("Nro_Banios")]
         [Display(Name = "Nro. de baños")]
         public int NroBanios { get; set; }
 
         [Display(Name = "Propietario")]
         public int IdPropietario { get; set; }
+
+        [ForeignKey(nameof(IdPropietario))]
+        public Propietario? Propietario { get; set; }
 
         public bool Habilitado { get; set; } = true;
 
